e49f400cf2a2a97affb9a0bda89a15ab2e40fee0
KDSigel <heykarl@gmail.com>
2022-01-31 13:46:27 -0800

    tests are passing

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,4 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+SELECT first_name, last_name, customer_list.address, customer_list.city FROM customer
+LEFT JOIN customer_list ON customer_list.id = customer.customer_id
